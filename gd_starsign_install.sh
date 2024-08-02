b81176e5287c11aee680c3681dd3e719736b53fc
Rafael Sahb <sahb@tutanota.com>
2024-08-02 17:14:58 -0300

    Updating URL for libssl1
    The link was broken, I am sending the update url.

--- gd_starsign_install.sh
+++ gd_starsign_install.sh
@@ -7,7 +7,7 @@ sudo apt install libwxbase3.0-0v5 libwxgtk3.0-gtk3-0v5 -y
 cd ~/Downloads
 mkdir gd_starsign
 cd gd_starsign
-wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb https://safesign.gdamericadosul.com.br/content/SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.rar 
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb https://safesign.gdamericadosul.com.br/content/SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.rar 
 sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb
 file-roller -h SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.rar
 sudo dpkg -i SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.deb
