d8e773cc4ea642456d7076ff4c9301ed8b967165
Renato Ramalho <60747312+BassHero@users.noreply.github.com>
2026-03-04 23:16:31 -0300

    Update token_gd_starsign_ubuntu_24.04.sh

--- token_gd_starsign_ubuntu_24.04.sh
+++ token_gd_starsign_ubuntu_24.04.sh
@@ -1,58 +1,100 @@
 #!/bin/bash
 
-# Variáveis das Urls padrão do debian e do ubuntu
-	debian_url=http://ftp.us.debian.org/debian/pool/main
-	ubuntu_url=http://security.ubuntu.com/ubuntu/pool/main
-	ubuntu_archive_url=http://archive.ubuntu.com/ubuntu/pool/universe
-	ubuntu_archive_main_url=http://archive.ubuntu.com/ubuntu/pool/main/o/openssl
+echo "
+	###########################################################
+	################# Safesign Linux Installer ################
+	###########################################################
 	
-# libs que não tem nos repositórios padrão do Ubuntu
-# Variáveis dos arquivos .deb
-	# libjpeg62_turbo_=libjpeg62-turbo_2.1.5-2+b2_amd64.deb
-	libjpeg62_turbo_=libjpeg62-turbo_2.1.5-4_amd64.deb
-	libssl1_1_=libssl1.1_1.1.1f-1ubuntu2.24_amd64.deb	
-	libtiff5_=libtiff5_4.1.0+git191117-2ubuntu0.20.04.14_amd64.deb
-	libwebp6_=libwebp6_0.6.1-2ubuntu0.20.04.3_amd64.deb
-	libwxbase3_0_=libwxbase3.0-0v5_3.0.5.1+dfsg-4_amd64.deb
-	libwxgtk3_0_=libwxgtk3.0-gtk3-0v5_3.0.5.1+dfsg-4_amd64.deb
+	Instalador do Certificado digital no Ubuntu e derivados.
 	
-# Variáveis dos links de download das libs .deb
-	libjpeg62_turbo=$debian_url/libj/libjpeg-turbo/$libjpeg62_turbo_
-	libssl1_1=$ubuntu_archive_main_url/$libssl1_1_
-	libtiff5=$ubuntu_url/t/tiff/$libtiff5_
-	libwebp6=$ubuntu_url/libw/libwebp/$libwebp6_
-	libwxbase3_0=$ubuntu_archive_url/w/wxwidgets3.0/$libwxbase3_0_
-	libwxgtk3_0=$ubuntu_archive_url/w/wxwidgets3.0/$libwxgtk3_0_
-
-# Aquivo Safesign baixado e descompactado
-	safesign_url=https://safesign.gdamericadosul.com.br/content
-	safesign_rar=SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.rar  
-	# safesign_zip=SafeSign_IC_Standard_Linux_ub2204_3.8.0.0_AET.000.zip
-	safesign_dl=$safesign_url/$safesign_rar
-	# safesign_deb=SafeSign\ IC\ Standard\ Linux\ 3.8.0.0-AET.000\ ub2204\ x86_64.deb
-	safesign_deb=SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.deb
-
-# Instalando as bibliotecas que já tem nos repositórios padrão do ubuntu
-	sudo apt install opensc libgdk-pixbuf2.0-0 -y
-
-# Baixando as bibliotecas que não tem nos repositórios padrão do ubuntu
-	mkdir ~/Downloads/token_gd_starsign && cd ~/Downloads/token_gd_starsign
-	wget -nc $libjpeg62_turbo $libssl1_1 $libtiff5 $libwebp6 $libwxbase3_0 $libwxgtk3_0 $safesign_dl
-
-# Instalando as bibliotecas que não tem nos repositórios padrão do ubuntu
-	sudo dpkg -i $libwebp6_ $libtiff5_ $libjpeg62_turbo_ $libwxbase3_0_ $libwxgtk3_0_ $libssl1_1_
-
-# Descompactando e instalando o safesign
-	#unzip -o $safesign_zip
-	file-roller -h $safesign_rar
+	Página oficial de download do Safesign:
+	https://safesign.gdamericadosul.com.br/
 	
-	rm $safesign_rar	
-	sudo dpkg -i SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.deb
-	# sudo dpkg -i SafeSign\ IC\ Standard\ Linux\ 3.8.0.0-AET.000\ ub2204\ x86_64.deb
+	Github deste Programa:
+	https://github.com/BassHero/gd_starsign_driver
+	
+	Autor: Renato Ramalho
+	email para contato: renatoramalho1986@gmail.com
+	
+	Caso queria me fazer uma doação:
+
+	PIX: renatoramalho232@gmail.com  
+
+	Bitcoin(bipatag): $renatoramalho
+
+	Bitcoin: bc1qx2mm6d4uxt72czrwpk8xh73vavzt2kmxcuwx7l
+
+	Bitcoin(lighting):
+		lnbc1p5clyfppp5q5fgduca0gnkluypaq7dj3nlmdvnkwvg5z8rv2lagrda4as79q3qdqqcqzzsxqrrsssp56jmjfydk8hs2erm4d02k0mc9kye79slmtdnhxk64m37zmwhy3mhs9qxpqysgq9v20kv6ympgtpz3uzmkfclzf3nszvysj8d46gxqwv7ew9vy0jlf8sv0sw4jx4mwxce0z8adgjk4tycs9cuc36qe6vejzcp9xy00rq2cpnu32c3
+
+===========================================================================
+
+	
+	###########################################################
+	###########################################################
+	###########################################################
+"
+
+# Pacotes .deb
+
+LIBWEBP6=libwebp6_0.6.1-2.1+deb11u2_amd64.deb
+
+LIBTIFF5=libtiff5_4.2.0-1+deb11u7_amd64.deb
+
+LIBJPEG62=libjpeg62-turbo_2.1.5-4_amd64.deb
+
+LIBWXBASE=libwxbase3.0-0v5_3.0.5.1+dfsg-2_amd64.deb
+
+LIBWXGTK=libwxgtk3.0-gtk3-0v5_3.0.5.1+dfsg-2_amd64.deb
+
+LIBSSL1=libssl1.1_1.1.1w-0+deb11u5_amd64.deb
+
+SAFESIGN=SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.rar
+
+SAFESIGN_DEB=SafeSign_IC_Standard_Linux_3.7.0.0_AET.000_ub2004_x86_64.deb
+
+# Links dos pacotes:
+
+LIBWEBP6_URL=http://ftp.us.debian.org/debian/pool/main/libw/libwebp/$LIBWEBP6
+
+LIBTIFF5_URL=http://security.debian.org/debian-security/pool/updates/main/t/tiff/$LIBTIFF5
+
+LIBJPEG62_TURBO_URL=http://ftp.us.debian.org/debian/pool/main/libj/libjpeg-turbo/$LIBJPEG62
+
+LIBWXBASE_URL=http://ftp.us.debian.org/debian/pool/main/w/wxwidgets3.0/$LIBWXBASE
+
+LIBWXGTK_URL=http://ftp.us.debian.org/debian/pool/main/w/wxwidgets3.0/$LIBWXGTK
+
+LIBSSL1_URL=http://security.debian.org/debian-security/pool/updates/main/o/openssl/$LIBSSL1
+
+SAFESIGN_URL=https://safesign.gdamericadosul.com.br/content/$SAFESIGN
+
+# Instalando as bibliotecas que estão nos repositórios padrão do Ubuntu.
+
+sudo apt install pcscd libgdk-pixbuf2.0-0 unrar -y
+
+# Baixando as bibliotecas que estão nos repositórios padrão do Ubuntu e o Safesign.
+
+wget -nc $LIBWEBP6_URL $LIBTIFF5_URL $LIBJPEG62_TURBO_URL $LIBWXBASE_URL $LIBWXGTK_URL $LIBSSL1_URL $SAFESIGN_URL
+
+# Instalando as bibliotecas baixadas.
+
+sudo dpkg -i $LIBWEBP6 $LIBTIFF5 $LIBJPEG62_TURBO $LIBWXBASE $LIBWXGTK $LIBSSL1
+
+# Extraindo e instalando o safesign
+
+unrar x $SAFESIGN
+
+sudo dpkg -i $SAFESIGN_DEB
 
 # Iniciando e habilitando o serviço pcscd
-	systemctl start pcscd.service
-	systemctl enable pcscd.service
 
-# Verificando se o token foi reconhecido
-	tokenadmin
+systemctl start pcscd.service
+
+systemctl enable pcscd.service
+
+# Iniciando o token e verificando se foi reconhecido
+
+tokenadmin
+
+
